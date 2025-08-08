612531ce0e030239432c5af7302a369f269f0877
Hasan Mohsin <hasan@hasali.dev>
2025-08-08 07:53:10 +0100

    Enable text compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import compressor from "astro-compressor";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [compressor()],
   vite: {
     // @ts-expect-error
     plugins: [tailwindcss()],
